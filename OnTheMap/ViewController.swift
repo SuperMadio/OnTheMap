d9b2a0bdb638ae8cd2f5db8412b35f8decb6c698
SuperMadio <30291256+SuperMadio@users.noreply.github.com>
2019-01-01 19:53:47 +0300

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Super Madio on 25/04/1440 AH.
+//  Copyright © 1440 Super Madio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
